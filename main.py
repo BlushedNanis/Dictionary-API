1286beee7f4141a9de0636a8f06a119e0f20a87d
BlushedNanis <luis_danieltg@outlook.com>
2024-03-27 02:16:22 -0400

    Added dictionary file and functionality to API endpoint

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+from pandas import read_csv
 
 app = Flask(__name__)
 
@@ -11,10 +11,14 @@ def home():
 
 @app.route("/api/v1/<word>")
 def api(word):
-    definition = "SUN"
-    return {"definition": definition,
-            "word": word}
-    
+    df = read_csv("dictionary.csv")
+    definition = df.loc[df["word"] == word.lower()]["definition"].squeeze()
+    print(type(definition))
+    if type(definition) == str:
+        return {"definition": definition,
+                "word": word}
+    else:
+        return "Please, provide a valid word."
 
 if __name__ == "__main__":
     app.run(debug=True)
